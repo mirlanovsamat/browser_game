014fd9100e85e581cd520d8d315b68d9bd3a9fda
Samat Mirlanov <mirlanov.sam@gmail.com>
2022-12-09 16:51:29 +0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
-import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    credentials: true,
+  })
   const PORT = 3000;
   await app.listen(PORT);
   console.log(`App is running on: ${ await app.getUrl() }`);
