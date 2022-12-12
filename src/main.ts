55327c6748b397584c9f26af57c57f9c4bdcc01b
Samat <mirlanov.sam@gmail.com>
2022-12-12 16:36:00 +0600

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   const configService = app.get(ConfigService);
   const PORT = configService.get<number>('PORT') || 3000;
   app.enableCors({
     origin: '*',
     credentials: true,
   })
-  await app.listen(PORT);
   app.setGlobalPrefix('api')
+  await app.listen(PORT);
   console.log(`App is running on: ${ await app.getUrl() }`);
 }
 bootstrap();
--- src/modules/user/dto/createUser.dto.ts
+++ src/modules/user/dto/createUser.dto.ts
@@ -1,9 +1,11 @@
+import { Transform } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateUserDto{
     
     @IsString()
     @IsNotEmpty()
+    @Transform(({value}) => value?.trim())
     name: string;
 
 }
